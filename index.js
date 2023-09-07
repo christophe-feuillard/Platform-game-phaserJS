421b9e246765e3fca513e59798b375f0e4a21b63
christophef <christophe.feuillard78320@gmail.com>
2023-09-07 22:40:43 +0200

    changement d'asset

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 900,
     height: 600,
     physics: {
         default: 'arcade', // moteur phyisque le plus simple, le plus léger et le mieux adapter pour des jeux en 2D
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 400 },
             debug: false
         }
     },
@@ -24,16 +24,13 @@ var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
-var isAttacking = false;
-console.log(isAttacking);
-
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
+    this.load.image('background', 'assets/background.jpg');
+    this.load.image('rockplatform', 'assets/rockplatform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('soldier', 'assets/persosprite.png', { frameWidth: 64, frameHeight: 48 });
@@ -43,25 +40,23 @@ function preload ()
 function create ()
 {
     //  A simple background for our game
-    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'background');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
 
     //  Here we create the ground.
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(400, 568, 'rockplatform').setScale(1).refreshBody();
 
     //  Now let's create some ledges
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+    // platforms.create(600, 400, 'rockplatform');
 
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'soldier'); // les sprites c'est des images avec des animations
 
     //  Player physics properties. Give the little guy a slight bounce.
-    player.setBounce(0.1);
+    player.setBounce(0);
     player.setCollideWorldBounds(true);
 
     //  Our player animations, turning, walking left and walking right.
@@ -85,16 +80,6 @@ function create ()
         repeat: -1
     });
 
-    this.input.keyboard.on('keydown-E', function () {
-        console.log('test');
-        isAttacking = true;
-    });
-
-    this.input.keyboard.on('keyup-E', function () {
-        console.log('test');
-        isAttacking = false;
-    });
-
     this.anims.create({
         key: 'attack',
         frames: this.anims.generateFrameNumbers('soldier_attack', { start: 0, end: 5 }),
@@ -105,34 +90,11 @@ function create ()
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    // stars = this.physics.add.group({
-    //     key: 'star',
-    //     repeat: 11,
-    //     setXY: { x: 12, y: 0, stepX: 70 }
-    // });
-
-    // stars.children.iterate(function (child) {
-
-    //     //  Give each star a slightly different bounce
-    //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    // });
-
-    // bombs = this.physics.add.group();
-
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
     //  Collide the player and the stars with the platforms 
     this.physics.add.collider(player, platforms); // ajoute la collision entre le joueur et les plateformes
-    // this.physics.add.collider(stars, platforms);
-    // this.physics.add.collider(bombs, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    // this.physics.add.overlap(player, stars, collectStar, null, this);
-
-    // this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update ()
@@ -153,60 +115,6 @@ function update ()
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
-    }
-
-    if (isAttacking) {
-        console.log(isAttacking);
-    // Jouez l'animation d'attaque
-    player.anims.play('attack', true);
-    } else {
-        // Jouez l'animation de marche normale
-        if (cursors.left.isDown) {
-            player.anims.play('left', true);
-        } else if (cursors.right.isDown) {
-            player.anims.play('right', true);
-        } else {
-            player.anims.play('turn');
-        }
+        player.setVelocityY(-250);
     }
 }
-
-// function collectStar (player, star)
-// {
-//     star.disableBody(true, true);
-
-//     //  Add and update the score
-//     score += 10;
-//     scoreText.setText('Score: ' + score);
-
-//     if (stars.countActive(true) === 0)
-//     {
-//         //  A new batch of stars to collect
-//         stars.children.iterate(function (child) {
-
-//             child.enableBody(true, child.x, 0, true, true);
-
-//         });
-
-//         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-//         var bomb = bombs.create(x, 16, 'bomb');
-//         bomb.setBounce(1);
-//         bomb.setCollideWorldBounds(true);
-//         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-//         bomb.allowGravity = false;
-
-//     }
-// }
-
-// function hitBomb (player, bomb)
-// {
-//     this.physics.pause();
-
-//     player.setTint(0xff0000);
-
-//     player.anims.play('turn');
-
-//     gameOver = true;
-// }
